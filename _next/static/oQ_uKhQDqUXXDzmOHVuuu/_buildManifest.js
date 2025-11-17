9a2332476ce73641298a31a3f68ad461653af953
ankitpyc <ankitmishra471@gmail.com>
2025-11-17 20:18:16 +0530

    Updates

--- _next/static/oQ_uKhQDqUXXDzmOHVuuu/_buildManifest.js
+++ _next/static/oQ_uKhQDqUXXDzmOHVuuu/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/modern-portfolio//_next/:path+",
+        "destination": "/modern-portfolio/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
